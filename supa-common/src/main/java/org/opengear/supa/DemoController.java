41d78d40c6e1bfe0d0341c6f518776923cfbb993
weian404 <weian404@gmail.com>
2025-03-05 21:42:39 +0800

    add controller response wrapper

--- supa-common/src/main/java/org/opengear/supa/DemoController.java
+++ supa-common/src/main/java/org/opengear/supa/DemoController.java
@@ -4,15 +4,17 @@ import org.opengear.supa.http.IgnoreWrapResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+
 @RestController
 public class DemoController {
 
     @IgnoreWrapResult
     @RequestMapping(value = "/test")
     public Object test() {
-        return new Person("jacks");
+        return new Person("jacks",Long.MAX_VALUE,Instant.now());
     }
 
-    record Person(String name) {}
+    record Person(String name, Long l, Instant instant) {}
 
 }
---
+++ supa-common/src/main/java/org/opengear/supa/exception/UniException.java
@@ -0,0 +1,22 @@
+package org.opengear.supa.exception;
+
+import org.opengear.supa.common.UniResult;
+
+public class UniException  extends RuntimeException {
+    private String code;
+    private Object data;
+    protected UniException(String code,Object data,Throwable throwable,String message) {
+        super(message,throwable);
+        this.code = code;
+        this.data = data;
+    }
+
+    public static UniException of(String code, Object data,Throwable throwable, String message) {
+        return new UniException(code,data,throwable,message);
+    }
+
+    public UniResult<?> toUniResult() {
+        return UniResult.of(code,data,this,getMessage());
+    }
+
+}

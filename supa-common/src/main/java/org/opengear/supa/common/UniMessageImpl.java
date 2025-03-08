e783fa9f9951a2aaf78b0945d84a76d0e3d1c4b1
weian404 <weian404@gmail.com>
2025-03-08 09:27:18 +0800

    add controller response wrapper

--- supa-common/src/main/java/org/opengear/supa/common/UniMessageImpl.java
+++
@@ -1,8 +0,0 @@
-package org.opengear.supa.common;
-
-public class UniMessageImpl implements UniMessage {
-    private String message;
-    public UniMessageImpl(String message) {
-        this.message = message;
-    }
-}
